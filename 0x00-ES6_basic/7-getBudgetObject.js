c7355587e24595acd647430225b635ab6324cbe3
Vulcanric <jneric49@gmail.com>
2024-06-26 23:05:02 +0100

    Used js shorthand syntax for object properties value

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+// Use Shorthand syntax for object property's value
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
