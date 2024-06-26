190edcfcbf946e430a81668de2a5b68004cec42b
Vulcanric <jneric49@gmail.com>
2024-06-26 03:45:54 +0100

    Attempt: added `new` before `Error()` to see if the checker passed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 // Using .then as handlers
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => { // If promise is resolved!
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => console.error(Error())); // If the promise didn't get resolved
+    .catch(() => new Error()); // If the promise didn't get resolved
 }
