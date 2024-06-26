863fc354f68bce1895126a04ff0ed272c3549fd0
Vulcanric <jneric49@gmail.com>
2024-06-26 06:26:39 +0100

    Fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -18,5 +18,5 @@ export default function handleProfileSignup() {
 
       console.log(body, firstName, lastName);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.error('Signup system offline'));
 }
