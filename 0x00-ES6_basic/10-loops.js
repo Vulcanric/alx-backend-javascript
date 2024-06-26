f780882d43bb71ea0bd2c49e041fdfd278ceac65
Vulcanric <jneric49@gmail.com>
2024-06-27 00:51:00 +0100

    Used ES6 For/of functionality

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    const newValue = `${appendString}${value}`;
+    newArray.push(newValue);
+  }
+
+  return newArray;
+}
