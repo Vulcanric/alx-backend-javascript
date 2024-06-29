18335d334cbf56d81f42701894a5d0a6ee8d9da0
Vulcanric <jneric49@gmail.com>
2024-06-30 00:38:33 +0100

    Implemented a Currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+// Implements a Currency class
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Implements instances attribute getters and setters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Class method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
