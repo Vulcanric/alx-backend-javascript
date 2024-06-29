3be15e628765526716a9000aaed42d7362891bc8
Vulcanric <jneric49@gmail.com>
2024-06-29 22:58:21 +0100

    Moved main files into test directory

---
+++ 0x00-ES6_basic/test/2-main.js
@@ -0,0 +1,7 @@
+
+
+import getNeighborhoodsList from './2-arrow';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
