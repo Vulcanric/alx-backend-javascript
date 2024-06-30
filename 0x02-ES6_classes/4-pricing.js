9cf4d566465cb6f7196061427594e5c16a0706f5
Vulcanric <jneric49@gmail.com>
2024-06-30 01:16:17 +0100

    Implemented a new class, Pricing, that utilizes the utilizes class Currency

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,29 @@
+// Implements a new class, Pricing that utilizes class Currency
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Implements getters and setters for the instance attributes defined above
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // A new method that returns the attributes in a styled format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
