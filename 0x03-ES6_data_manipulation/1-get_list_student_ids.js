f0accd17f8395427544a597aa0b3045968d21118
Vulcanric <jneric49@gmail.com>
2024-07-03 03:43:05 +0100

    Implemented mapping on each array elements

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+// Implement array mapping
+/*
+ * Array.prototype.map - An iterative method that calls a function on every element
+ * of the array it's being called on, returning a new array with the computed elements
+ */
+export default function getListStudentIds(listOfStudents) {
+  // List of students is an array of student objects
+  if (typeof listOfStudents !== 'object') {
+    return [];
+  }
+  return listOfStudents.map(({ id }) => id); // Using obj destructuring to obtain obj id
+}
