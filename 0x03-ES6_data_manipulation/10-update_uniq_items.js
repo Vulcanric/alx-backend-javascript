7ef800c97f4ec3ceb690d2ad4308930865977272
Vulcanric <jneric49@gmail.com>
2024-07-06 04:26:13 +0100

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,20 @@
+/*
+ * Defined a function, `updateUniqueItems` that returns an updated map for all items
+ * with initial quantity at 1.
+ *
+ * It accepts a map as an argument. The map it accepts for argument is similar to the map created
+ * in the file `9-groceries_list.js`.
+ *
+ * For each entry of the map where the quantity is 1, it updates the quantity to 100.
+ * If updating the quantity isn't possible (argument is not a map) the error
+ * `Cannot process` is thrown.
+ */
+function updateUniqueItems(map) {
+  map.forEach((value, key) => {
+    if (value === 1) { // Update mapping where value is 1
+      map.set(key, 100);
+    }
+  });
+}
+
+export default updateUniqueItems;
